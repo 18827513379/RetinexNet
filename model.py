fcd8e4f9952d81bac08f49d1a24de161eae6c4ce
18827513379 <50547967+18827513379@users.noreply.github.com>
2019-09-15 19:41:07 +0800

    Update model.py

--- model.py
+++ model.py
@@ -10,33 +10,34 @@ import numpy as np
 
 from utils import *
 
-def concat(layers):
-    return tf.concat(layers, axis=3)
+def concat(layers)://矩阵拼接，按第4维度上连，应该是指通道的那个维度（B,W,H,C）
+    return tf.concat(layers, axis=3)//返回一个多维矩阵
 
-def DecomNet(input_im, layer_num, channel=64, kernel_size=3):
-    input_max = tf.reduce_max(input_im, axis=3, keepdims=True)
-    input_im = concat([input_max, input_im])
-    with tf.variable_scope('DecomNet', reuse=tf.AUTO_REUSE):
+def DecomNet(input_im, layer_num, channel=64, kernel_size=3)://定义分解网络，参数有输入，卷积层数量，滤波器通个数，卷积核尺寸
+    input_max = tf.reduce_max(input_im, axis=3, keepdims=True)//将输入图片减少到3维 255*255*3？
+    input_im = concat([input_max, input_im])//
+    with tf.variable_scope('DecomNet', reuse=tf.AUTO_REUSE)://在一个 variable_scope 作用域下内共享一些变量
+//第一个卷积层提取特征，
         conv = tf.layers.conv2d(input_im, channel, kernel_size * 3, padding='same', activation=None, name="shallow_feature_extraction")
-        for idx in range(layer_num):
+        for idx in range(layer_num)://循环中有5个卷积层，做卷积时为什么要加RELU，这几个层叫激活层？
             conv = tf.layers.conv2d(conv, channel, kernel_size, padding='same', activation=tf.nn.relu, name='activated_layer_%d' % idx)
         conv = tf.layers.conv2d(conv, 4, kernel_size, padding='same', activation=None, name='recon_layer')
-
-    R = tf.sigmoid(conv[:,:,:,0:3])
+//最后一个卷积层，通道数为什么是4，没有用到激活函数，叫重建层？
+    R = tf.sigmoid(conv[:,:,:,0:3]) // 分解为R和L分量
     L = tf.sigmoid(conv[:,:,:,3:4])
 
     return R, L
 
-def RelightNet(input_L, input_R, channel=64, kernel_size=3):
-    input_im = concat([input_R, input_L])
+def RelightNet(input_L, input_R, channel=64, kernel_size=3)://增强网络，通道数64，卷积核尺寸3*3
+    input_im = concat([input_R, input_L])//恢复为原来的图像形式，
     with tf.variable_scope('RelightNet'):
         conv0 = tf.layers.conv2d(input_im, channel, kernel_size, padding='same', activation=None)
         conv1 = tf.layers.conv2d(conv0, channel, kernel_size, strides=2, padding='same', activation=tf.nn.relu)
         conv2 = tf.layers.conv2d(conv1, channel, kernel_size, strides=2, padding='same', activation=tf.nn.relu)
         conv3 = tf.layers.conv2d(conv2, channel, kernel_size, strides=2, padding='same', activation=tf.nn.relu)
-        
-        up1 = tf.image.resize_nearest_neighbor(conv3, (tf.shape(conv2)[1], tf.shape(conv2)[2]))
-        deconv1 = tf.layers.conv2d(up1, channel, kernel_size, padding='same', activation=tf.nn.relu) + conv2
+        //提取图像的局部特征
+        up1 = tf.image.resize_nearest_neighbor(conv3, (tf.shape(conv2)[1], tf.shape(conv2)[2]))//加入最邻近插值
+        deconv1 = tf.layers.conv2d(up1, channel, kernel_size, padding='same', activation=tf.nn.relu) + conv2//跳跃连接
         up2 = tf.image.resize_nearest_neighbor(deconv1, (tf.shape(conv1)[1], tf.shape(conv1)[2]))
         deconv2= tf.layers.conv2d(up2, channel, kernel_size, padding='same', activation=tf.nn.relu) + conv1
         up3 = tf.image.resize_nearest_neighbor(deconv2, (tf.shape(conv0)[1], tf.shape(conv0)[2]))
@@ -44,35 +45,37 @@ def RelightNet(input_L, input_R, channel=64, kernel_size=3):
         
         deconv1_resize = tf.image.resize_nearest_neighbor(deconv1, (tf.shape(deconv3)[1], tf.shape(deconv3)[2]))
         deconv2_resize = tf.image.resize_nearest_neighbor(deconv2, (tf.shape(deconv3)[1], tf.shape(deconv3)[2]))
-        feature_gather = concat([deconv1_resize, deconv2_resize, deconv3])
-        feature_fusion = tf.layers.conv2d(feature_gather, channel, 1, padding='same', activation=None)
+        feature_gather = concat([deconv1_resize, deconv2_resize, deconv3])//连接到
+        feature_fusion = tf.layers.conv2d(feature_gather, channel, 1, padding='same', activation=None) //1*1的卷积核
         output = tf.layers.conv2d(feature_fusion, 1, 3, padding='same', activation=None)
     return output
 
 class lowlight_enhance(object):
     def __init__(self, sess):
         self.sess = sess
-        self.DecomNet_layer_num = 5
+        self.DecomNet_layer_num = 5//分解网络5层
 
         # build the model
+        //神经网络构建graph的时候在模型中的占位，此时并没有把要输入的数据传入模型，它只会分配必要的内存
+        //此函数可以理解为形参，用于定义过程，在执行的时候再赋具体的值
         self.input_low = tf.placeholder(tf.float32, [None, None, None, 3], name='input_low')
         self.input_high = tf.placeholder(tf.float32, [None, None, None, 3], name='input_high')
 
-        [R_low, I_low] = DecomNet(self.input_low, layer_num=self.DecomNet_layer_num)
+        [R_low, I_low] = DecomNet(self.input_low, layer_num=self.DecomNet_layer_num)//返回分解网分解的分量
         [R_high, I_high] = DecomNet(self.input_high, layer_num=self.DecomNet_layer_num)
         
-        I_delta = RelightNet(I_low, R_low)
+        I_delta = RelightNet(I_low, R_low)//返回增强后的I分量
 
-        I_low_3 = concat([I_low, I_low, I_low])
+        I_low_3 = concat([I_low, I_low, I_low])将这些分量连接为原来的维度？保持维度一致？
         I_high_3 = concat([I_high, I_high, I_high])
         I_delta_3 = concat([I_delta, I_delta, I_delta])
 
         self.output_R_low = R_low
         self.output_I_low = I_low_3
         self.output_I_delta = I_delta_3
-        self.output_S = R_low * I_delta_3
+        self.output_S = R_low * I_delta_3//合成增强后的光照度
 
-        # loss
+        # loss//损失函数
         self.recon_loss_low = tf.reduce_mean(tf.abs(R_low * I_low_3 -  self.input_low))
         self.recon_loss_high = tf.reduce_mean(tf.abs(R_high * I_high_3 - self.input_high))
         self.recon_loss_mutal_low = tf.reduce_mean(tf.abs(R_high * I_low_3 - self.input_low))
@@ -103,8 +106,8 @@ class lowlight_enhance(object):
 
         print("[*] Initialize model successfully...")
 
-    def gradient(self, input_tensor, direction):
-        self.smooth_kernel_x = tf.reshape(tf.constant([[0, 0], [-1, 1]], tf.float32), [2, 2, 1, 1])
+    def gradient(self, input_tensor, direction)://创建一个常量
+        self.smooth_kernel_x = tf.reshape(tf.constant([[0, 0], [-1, 1]], tf.float32), [2, 2, 1, 1])//重塑形状，格式[2, 2, 1, 1]
         self.smooth_kernel_y = tf.transpose(self.smooth_kernel_x, [1, 0, 2, 3])
 
         if direction == "x":
@@ -113,18 +116,19 @@ class lowlight_enhance(object):
             kernel = self.smooth_kernel_y
         return tf.abs(tf.nn.conv2d(input_tensor, kernel, strides=[1, 1, 1, 1], padding='SAME'))
 
-    def ave_gradient(self, input_tensor, direction):
+    def ave_gradient(self, input_tensor, direction)://平均池化
         return tf.layers.average_pooling2d(self.gradient(input_tensor, direction), pool_size=3, strides=1, padding='SAME')
 
     def smooth(self, input_I, input_R):
-        input_R = tf.image.rgb_to_grayscale(input_R)
-        return tf.reduce_mean(self.gradient(input_I, "x") * tf.exp(-10 * self.ave_gradient(input_R, "x")) + self.gradient(input_I, "y") * tf.exp(-10 * self.ave_gradient(input_R, "y")))
-
+        input_R = tf.image.rgb_to_grayscale(input_R)//将一个或多个图像从RGB转换为灰度
+        return tf.reduce_mean(self.gradient(input_I, "x") * tf.exp(-10 * self.ave_gradient(input_R, "x")) 
+                              + self.gradient(input_I, "y") * tf.exp(-10 * self.ave_gradient(input_R, "y")))
+    //计算损失函数
     def evaluate(self, epoch_num, eval_low_data, sample_dir, train_phase):
         print("[*] Evaluating for phase %s / epoch %d..." % (train_phase, epoch_num))
 
         for idx in range(len(eval_low_data)):
-            input_low_eval = np.expand_dims(eval_low_data[idx], axis=0)
+            input_low_eval = np.expand_dims(eval_low_data[idx], axis=0)//用于扩展数组的形状,在0位置添加数据,数组形状变为0的位置是1.[1 ? ? ?]
 
             if train_phase == "Decom":
                 result_1, result_2 = self.sess.run([self.output_R_low, self.output_I_low], feed_dict={self.input_low: input_low_eval})
@@ -170,19 +174,19 @@ class lowlight_enhance(object):
                 batch_input_low = np.zeros((batch_size, patch_size, patch_size, 3), dtype="float32")
                 batch_input_high = np.zeros((batch_size, patch_size, patch_size, 3), dtype="float32")
                 for patch_id in range(batch_size):
-                    h, w, _ = train_low_data[image_id].shape
-                    x = random.randint(0, h - patch_size)
+                    h, w, _ = train_low_data[image_id].shape//图片形状大小
+                    x = random.randint(0, h - patch_size)//255-48?
                     y = random.randint(0, w - patch_size)
             
-                    rand_mode = random.randint(0, 7)
+                    rand_mode = random.randint(0, 7)//7种变换模式
                     batch_input_low[patch_id, :, :, :] = data_augmentation(train_low_data[image_id][x : x+patch_size, y : y+patch_size, :], rand_mode)
                     batch_input_high[patch_id, :, :, :] = data_augmentation(train_high_data[image_id][x : x+patch_size, y : y+patch_size, :], rand_mode)
                     
                     image_id = (image_id + 1) % len(train_low_data)
                     if image_id == 0:
                         tmp = list(zip(train_low_data, train_high_data))
-                        random.shuffle(list(tmp))
-                        train_low_data, train_high_data  = zip(*tmp)
+                        random.shuffle(list(tmp))//让训练数据集中的数据打乱顺序，然后一个挨着一个地(for i in indices)生成训练数据对
+                        train_low_data, train_high_data  = zip(*tmp)//从tmp中依次取一个元组，组成一个元组
 
                 # train
                 _, loss = self.sess.run([train_op, train_loss], feed_dict={self.input_low: batch_input_low, \
